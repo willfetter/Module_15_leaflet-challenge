613e51bc6409c0c160bda03944c69d9076e7e146
William Fetter <willfetter@gmail.com>
2025-02-18 07:23:25 -0500

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,14 +4,6 @@ var defaultMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
   attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
-// Greyscale layer
-var grayscale = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.{ext}', {
-  minZoom: 0,
-  maxZoom: 20,
-  attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  ext: 'png'
-});
-
 // World imagery layer
 var aerial = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
   minZoom: 0,
@@ -25,21 +17,11 @@ var topoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
-// Watercolor Layer
-var waterColor = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}', {
-  minZoom: 1,
-  maxZoom: 16,
-  attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  ext: 'jpg'
-});
-
 // Make a basemap object
 let basemaps = {
   Default: defaultMap,
-  Grayscale: grayscale,
   Aerial: aerial,
-  TopMap: topoMap,
-  "Water Color": waterColor
+  TopMap: topoMap
 };
 
 // Create the map object with center and zoom options. 
@@ -47,7 +29,7 @@ let basemaps = {
 var myMap = L.map("map", {
   center: [36.7783, -119.4179],
   zoom: 5,
-  layers: [grayscale, aerial, topoMap, waterColor, defaultMap]
+  layers: [defaultMap, aerial, topoMap]
 });
 
 // Add default tile layer to the map.
